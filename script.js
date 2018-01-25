d28bfd9266549a722aa6ef9de92159e3955bcb73
Vra3 <vrafal333@gmail.com>
2018-01-25 22:56:01 +0100

    1.1
    good for mobiles

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
 });
 
-$("ul").on("click", "span", function(event){
+$("ul").on("click", "#removeIcon", function(event){
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   })
@@ -12,7 +12,7 @@ $("ul").on("click", "span", function(event){
 $("input[type=text]").on("keypress", function(Event){
   if(Event.which === 13){
     var todoText = $(this).val();
-    $("ul").append("<li><span><i class=\"fa fa-trash\"></i></span>" + todoText +"</li>");
+    $("ul").append("<li><span id=\"removeIcon\"><i class=\"fa fa-trash\"></i></span> " + todoText +"</li>");
     $("input").val("");
   }
 });
@@ -20,3 +20,9 @@ $("input[type=text]").on("keypress", function(Event){
 $(".fa-plus").on("click", function(){
   $("input").fadeToggle(200);
 })
+
+$("ul").on("click", "#importantIcon", function(event){
+  $(this).parent().toggleClass("strong");
+  $(this).toggleClass("selectedStar");
+  event.stopPropagation();
+});
